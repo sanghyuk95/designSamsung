4502a4b6aa9402a931b45694f9fa9c174881269b
Burny95 <shskse5@gmail.com>
2023-03-09 18:48:54 +0900

    03-09 메인배너

--- js/index.js
+++ js/index.js
@@ -12,4 +12,36 @@ window.onload = function () {
     },
     offset: "-10%",
   });
+
+  var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 2000,
+    },
+    loop: true,
+    pagination: {
+      el: ".index-center",
+      type: "progressbar",
+    },
+    navigation: {
+      prevEl: ".fa-arrow-left",
+      nextEl: ".fa-arrow-right",
+    },
+  });
+
+  const $play = document.querySelector(".play");
+  const $indexLeft = document.querySelector(".index-left");
+
+  swiper.on("slideChange", () => {
+    $indexLeft.textContent = `${swiper.realIndex + 1}`;
+  });
+
+  $play.addEventListener("click", () => {
+    $play.classList.toggle("fa-pause");
+    $play.classList.toggle("fa-angle-right");
+    if ($play.classList.contains("fa-angle-right")) {
+      swiper.autoplay.pause();
+    } else {
+      swiper.autoplay.resume();
+    }
+  });
 };
